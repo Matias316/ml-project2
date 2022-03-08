2c7945be2d9aabbae19e662351017ab8d410f1c1
Matias Antiñanco <matias.antinanco@gmail.com>
2022-03-08 17:52:48 -0300

    Add relevance to countplot

--- exercise_2/plot_data.py
+++ exercise_2/plot_data.py
@@ -10,10 +10,18 @@ def display_count_plot(df1, df2):
         df2 (dataframe)
 
     """
+    # TODO - Improve function accuracy:
+    # This is not best approach to identify positive tweets:
+    # 1) It doesn't include keywords in other languages
+    # 2) It doesn't apply a semantic analysis
+    positive_tweets = ['excellent', 'best', 'awesome', 'fantastic', 'out of this world', 'superior', 'greatest', 'highest goalscorer', 'most assists']
+    df1['relevance'] = df1.apply(lambda row: 'Positive' if any([item in row['text'] for item in positive_tweets]) else 'Undetermined', axis = 1)
+    df2['relevance'] = df2.apply(lambda row: 'Positive' if any([item in row['text'] for item in positive_tweets]) else 'Undetermined', axis = 1)    
+
     fig, ax = plt.subplots(1, 2)
     ax[0].title.set_text('Messi')
     ax[1].title.set_text('Ronaldo')
-    sns.countplot(x='lang', ax=ax[0], data=df1)
-    sns.countplot(x='lang', ax=ax[1], data=df2)
+    sns.countplot(x='lang', ax=ax[0], data=df1, hue='relevance')
+    sns.countplot(x='lang', ax=ax[1], data=df2, hue='relevance')
 
     plt.show()
